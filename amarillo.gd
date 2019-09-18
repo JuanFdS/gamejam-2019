5f21d3101a46f8f2ab71d64c9320275a8c9313dd
juanFdS <juan9794@gmail.com>
2019-09-17 23:33:08 -0300

    bomba yogurt

--- amarillo.gd
+++ amarillo.gd
@@ -1,4 +1,15 @@
 extends "unit.gd"
 
+var dead = false;
+
 func fall():
 	return
+	
+func explode():
+	$AnimatedSprite.animation = 'die'
+	stopMoving()
+	dead = true
+
+func _process(delta):
+	if dead:
+		position.y -= 1
--- troll.gd
+++ troll.gd
@@ -6,6 +6,9 @@ func fall():
 	$AnimatedSprite.set_light_mask(524288)
 	$AnimationPlayer.play("fallAnimation")
 
+func explode():
+	return
+
 func _on_AnimationPlayer_animation_finished(anim_name):
 	if(anim_name == "fallAnimation"):
 		destroy()
